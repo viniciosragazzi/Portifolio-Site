c19d517f000f0913ba0e810cc3a1363ca5c261dc
viniciosragazzi <viniciosragazzzi@gmail.com>
2020-12-04 01:35:11 -0300

    01:35

---
+++ js/script.js
@@ -0,0 +1,42 @@
+let divt = document.querySelector(".div-t")
+let lside = document.querySelector(".lside")
+let fechar = document.querySelector(".fechar")
+let bar = document.querySelector(".bar")
+function abrirMenu(){
+    lside.style.position = "relative"
+    lside.style.transform = "translateY(0)"
+    bar.style.display = "none"
+    fechar.style.display = "flex"
+}
+function fecharMenu(){
+    lside.style.position = "absolute"
+    lside.style.transform = "translateY(-750px)"
+    bar.style.display = "flex"
+    fechar.style.display = "none"
+}
+
+
+function tamanhoTela(){
+    var windowWidth = window.innerWidth;
+  
+   if(windowWidth <= 768){
+    lside.style.position = "absolute"
+    lside.style.transform = "translateY(-850px)"
+    bar.style.display = "flex"
+    fechar.style.display = "none"
+    divt.classList.add("container")
+    }else{
+    lside.style.position = "fixed"
+    lside.style.transform = "translateY(0)"
+    bar.style.display = "none"
+    fechar.style.display = "none"
+    divt.classList.remove("container")
+    }
+  };
+  
+  tamanhoTela();
+  
+  window.addEventListener('resize', function(){
+      tamanhoTela();
+      
+  });
